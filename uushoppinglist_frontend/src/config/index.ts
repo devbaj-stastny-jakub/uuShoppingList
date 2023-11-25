ad7d84fe0af35bc7b24586fe501067af7b90a8bf
devbaj-stastny-jakub <kubalucky762@gmail.com>
2023-11-25 20:16:43 +0100

    :sparkles: Move fe to https

--- uushoppinglist_frontend/src/config/index.ts
+++ uushoppinglist_frontend/src/config/index.ts
@@ -1,6 +1,6 @@
 export * from "./theme"
 
 export const backend = {
-    base_url: "http://164.90.187.82:3001"
+    base_url: "https://164.90.187.82:3001"
     //base_url: "http://localhost:3001"
 }
