466255687d89077241afbb1af0247b12204b749c
devbaj-stastny-jakub <kubalucky762@gmail.com>
2023-11-25 20:06:09 +0100

    :sparkles: Add cert for server

--- uushoppinglist_backend/index.ts
+++ uushoppinglist_backend/index.ts
@@ -1,9 +1,11 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import {shoppingListRouter} from "./routes/shoppingList";
 import {shoppingListItemRouter} from "./routes/shoppingListItem";
 import cors from "cors"
 import {errorHandler} from "./middlewares";
 import {PrismaClient} from "@prisma/client";
+import * as fs from "fs";
+import * as https from "https";
 
 const app = express();
 const port = 3001;
@@ -15,8 +17,22 @@ app.use(express.json({limit: "50mb"}));
 app.use("/shoppingList", shoppingListRouter)
 app.use("/shoppingListItem", shoppingListItemRouter)
 
+app.get('/', (req,res)=>{
+    res.send("Hello from express server.")
+})
+
 app.use(errorHandler)
 
-app.listen(port, () => {
-    console.log(`Shopping list application is running on port ${port}.`);
-});
+https
+    .createServer(
+        // Provide the private and public key to the server by reading each
+        // file's content with the readFileSync() method.
+        {
+            key: fs.readFileSync("key.pem"),
+            cert: fs.readFileSync("cert.pem"),
+        },
+        app
+    )
+    .listen(3001, () => {
+        console.log("server is runing at port 3001");
+    });
--- uushoppinglist_frontend/src/config/index.ts
+++ uushoppinglist_frontend/src/config/index.ts
@@ -1,6 +1,6 @@
 export * from "./theme"
 
 export const backend = {
-    //base_url: "http://209.38.176.190:3001"
-    base_url: "http://localhost:3001"
+    base_url: "http://164.90.187.82:3001"
+    //base_url: "http://localhost:3001"
 }
